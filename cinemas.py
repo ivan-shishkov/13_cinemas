28996323bc68bae309377b1aaa32937316f8838d
Ivan Shishkov <mail-shishkov@yandex.ru>
2018-09-20 13:23:12 +0300

    Added the main script function

--- cinemas.py
+++ cinemas.py
@@ -1,20 +1,6 @@
-
-
-def fetch_afisha_page():
-    pass
-
-
-def parse_afisha_list(raw_html):
-    pass
-
-
-def fetch_movie_info(movie_title):
-    pass
-
-
-def output_movies_to_console(movies):
+def main():
     pass
 
 
 if __name__ == '__main__':
-    pass
+    main()
